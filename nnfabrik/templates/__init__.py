810fa5fdb2547c227eb756348f01d50b5922d464
Edgar Y. Walker <edgar.walker@gmail.com>
2020-11-12 04:04:25 +0000

    Expose new base table

--- nnfabrik/templates/__init__.py
+++ nnfabrik/templates/__init__.py
@@ -1,3 +1,3 @@
-from .trained_model import TrainedModelBase, DataInfoBase
+from .trained_model import TrainedModelBase, DataInfoBase, TrainedModelWithDataInfoBase
 from .scoring import ScoringBase
 from .transfer import TransferredTrainedModelBase
